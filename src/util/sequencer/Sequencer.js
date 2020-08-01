47867cb45674a5147574356fa440a6ed3751852d
Matt Nawrocki <mjnrock@umich.edu>
2020-08-01 19:17:23 -0400

    init

--- src/util/sequencer/Sequencer.js
+++ src/util/sequencer/Sequencer.js
@@ -41,45 +41,44 @@ export default class Sequencer {
     async bake() {
         return new Promise((resolve, reject) => Score.Create(this.children).then(score => resolve(score)));
 
+        //TODO This should return a <Composition> with one-to-many <Score> as .children, get child(...args) => <Score> that is appropriate -- e.g. i = 0 as default, child(10) => <Score> that is bounced <...Mixer>
+        //? Code below could be used as foundation after that refactor occurs
+        //     const proms = [];
 
-        //TODO Refactor rest of application to accommodate for this.children being an array instead of a <Mixer>
-        //? Code below should be used after that refactor occurs
-    //     const proms = [];
+        //     for(let child of this.children) {
+        //         if(child instanceof Mixer) {
+        //             proms.push(new Promise((resolve, reject) => Score.Create(child).then(score => resolve(score))));
+        //         } else if(child instanceof Sequencer) {
+        //             proms.push(child.bake());
+        //         }
+        //     }
 
-    //     for(let child of this.children) {
-    //         if(child instanceof Mixer) {
-    //             proms.push(new Promise((resolve, reject) => Score.Create(child).then(score => resolve(score))));
-    //         } else if(child instanceof Sequencer) {
-    //             proms.push(child.bake());
-    //         }
-    //     }
+        //     const size = {
+        //         width: 0,
+        //         height: 0,
+        //     };
+        //     const current = {
+        //         width: 0,
+        //         height: 0,
+        //     };
 
-    //     const size = {
-    //         width: 0,
-    //         height: 0,
-    //     };
-    //     const current = {
-    //         width: 0,
-    //         height: 0,
-    //     };
+        //     return Promise.all(proms).then(canvases => {
+        //         const cvs = document.createElement("canvas");
+        //         const ctx = cvs.getContext("2d");
 
-    //     return Promise.all(proms).then(canvases => {
-    //         const cvs = document.createElement("canvas");
-    //         const ctx = cvs.getContext("2d");
+        //         for(let canvas of canvases) {
+        //             size.width += canvas.width;
+        //             size.height += canvas.height;
+        //         }
 
-    //         for(let canvas of canvases) {
-    //             size.width += canvas.width;
-    //             size.height += canvas.height;
-    //         }
+        //         for(let canvas of canvases) {
+        //             ctx.drawImage(canvas, current.width, current.height);
 
-    //         for(let canvas of canvases) {
-    //             ctx.drawImage(canvas, current.width, current.height);
+        //             current.width += canvas.width;
+        //             current.height += canvas.height;
+        //         }
 
-    //             current.width += canvas.width;
-    //             current.height += canvas.height;
-    //         }
-
-    //         return cvs;
-    //     });
+        //         return cvs;
+        //     });
     }
 };
