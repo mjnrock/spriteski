8acf1e4bc76b90f7d57ba6d122cce59641a3d605
Matt Nawrocki <mjnrock@umich.edu>
2020-08-09 16:50:00 -0400

    init

--- src/util/sequencer/Score.js
+++ src/util/sequencer/Score.js
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from "uuid";
 import Mixer from "./Mixer";
 
 //TODO Create helper functions to perform hash lookups and facing lookups that would be stored in this.data
-//TODO Create accessor methods for the "current frame" and things like Elapsable
+//TODO Create accessor methods for the "current frame" and things like Elapsable (maybe make a "generator" that passes with a reference to the Score, so that multiple Elapsables can reference a single Score)
 
 export default class Score {
     constructor(mixer, { weight = 1, canvas, bounds = {}, data = {}} = {}) {
