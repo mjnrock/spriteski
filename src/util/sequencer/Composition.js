98fc491f1f20756dd7558ffd1684b3537319cb4a
Matt Nawrocki <mjnrock@umich.edu>
2020-08-19 08:39:56 -0400

    init

--- src/util/sequencer/Composition.js
+++ src/util/sequencer/Composition.js
@@ -1,10 +1,66 @@
 import { v4 as uuidv4 } from "uuid";
-import Score from "./Score";
+
+export const EnumFacing = {
+    D8TD: {
+        0: [ "left", "right", "body", ],    //"ground", "head", "corona" ],
+        45: [ "left", "right", "body", ],   //"ground", "head", "corona" ],
+        90: [ "left", "body", "right", ],   //"ground", "head", "corona" ],
+        135: [ "body", "left", "right", ],  //"ground", "head", "corona" ],
+        180: [ "body", "left", "right", ],  //"ground", "head", "corona" ],
+        225: [ "right", "body", "left", ],  //"ground", "head", "corona" ],
+        270: [ "right", "body", "left", ],  //"ground", "head", "corona" ],
+        315: [ "left", "right", "body", ],  //"ground", "head", "corona" ],
+    }
+};
 
 export default class Composition {
-    constructor({ scores = [] } = {}) {
-        this.id = uuidv4();
-        
-        this.scores = scores;
+    constructor(scores = [], { id } = {}) {
+        this.id = id || uuidv4();
+
+        this.scores = new Map(scores);
+    }
+
+    /**
+     * 
+     * @param {int} facing (degrees)
+     * @param {int} elapsedTime (ms)
+     * @returns [ { name, score, data: Score.get(@facing, @elapsedTime) }, ... ]
+     */
+    get(facing, elapsedTime, { lookup = EnumFacing.D8TD } = {}) {
+        const arr = [ ...this.scores.entries() ];
+        arr.sort(([ aname ], [ bname ]) => lookup[ facing ].indexOf(aname) - lookup[ facing ].indexOf(bname));
+
+        return new Set(arr.map(([ name, score ]) => ({ name, score, data: score.get(facing, elapsedTime) })));
+    }
+    
+    drawTo(canvas, { facing, elapsedTime, x, y, tx, ty }) {
+        const scores = this.get(facing, elapsedTime);
+
+        for(let { score, data } of [ ...scores.values() ]) {
+            const [ sx, sy ] = data.position;
+
+            if(sx !== void 0 && sy !== void 0) {
+                const ctx = canvas.getContext("2d");
+
+                if(tx !== void 0 && ty !== void 0) {
+                    x = tx * score.data.tile.width;
+                    y = ty * score.data.tile.height;
+                }
+
+                ctx.drawImage(
+                    score.canvas,
+                    sx,
+                    sy,
+                    score.data.tile.width,
+                    score.data.tile.height,
+                    x,
+                    y,
+                    score.data.tile.width,
+                    score.data.tile.height
+                );
+            }
+        }
+
+        return canvas;
     }
 };
